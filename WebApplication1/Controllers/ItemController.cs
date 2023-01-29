df76509ca1bc09138b4a54ab272d68d045e4d3d8
balajiamgithup <balaji.am2020@gmail.com>
2023-01-29 03:05:52 -0800

    first commit

---
+++ WebApplication1/Controllers/ItemController.cs
@@ -0,0 +1,157 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Runtime.InteropServices;
+using WebApplication1.Models.DTO;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : Controller
+    {
+        private readonly IitemRepository itemRepository;
+        private readonly IMapper mapper;
+        public ItemController(IitemRepository itemRepository, IMapper mapper)
+        {
+            this.itemRepository = itemRepository;
+            this.mapper = mapper;
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllitemAsync()
+        {
+            var items = await itemRepository.GetAllAsync();
+
+
+
+
+            // var itemssDTO = mapper.Map<List<Models.DTO.item>>(items);
+
+            var itemDTO = mapper.Map<List<Models.DTO.item>>(items);
+
+            return Ok(itemDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetItemAsync")]
+        public async Task<IActionResult> GetItemAsync(Guid id)
+        {
+            {
+                var item = await itemRepository.GetAsync(id);
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                var itemDTO = mapper.Map<Models.DTO.item>(item);
+                return Ok(itemDTO);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddItemAsync(Models.DTO.AddItemRequest addItemRequest)
+        {
+            //request to domain model
+            var item = new Models.Domain.Item()
+            {
+                name = addItemRequest.name,
+                price = addItemRequest.price
+
+            };
+
+            //pass details to repository
+
+            item = await itemRepository.AddAsync(item);
+            //Convert back to DTO
+
+            var itemDTO = new Models.DTO.item
+            {
+                itemId = item.itemId,
+                name = item.name,
+                price = item.price
+
+            };
+            return Ok(itemDTO);
+
+            //return CreatedAtAction(nameof(GetItemAsync), new { itemId = itemDTO.itemId }, itemDTO);
+        }
+
+        [HttpDelete]
+        [Route("{itemId:guid}")]
+        public async Task<IActionResult> DeleteItemAsymc(Guid itemId)
+        {
+            // Get item from database
+            var item = await itemRepository.DeleteAsync(itemId);
+
+
+            // If null NotFound
+            if (itemId == null)
+            {
+                return NotFound();
+            }
+
+
+            // Convert response back to DTO
+
+            var itemDTO = new Models.DTO.item
+            {
+                itemId = item.itemId,
+                name = item.name,
+                price = item.price
+
+            };
+
+
+            // return Ok response
+            return Ok(itemDTO);
+
+        }
+
+        [HttpPut]
+        [Route("{itemId:guid}")]
+        public async Task<IActionResult> UpdateItem([FromRoute]Guid itemId, [FromBody] Models.DTO.UpdateItemRequest updateItem)
+        {
+            // convert DTO to Domain model
+
+            var item = new Models.Domain.Item()
+            {
+                name = updateItem.name,
+                price = updateItem.price
+
+            };
+
+            //Update Item using repository
+
+              item= await itemRepository.UpdateAsync(itemId, item);
+
+
+            //if null then notfound
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            //convert domain back to DTO
+            var itemDTO = new Models.DTO.item
+            {
+                itemId = item.itemId,
+                name = item.name,
+                price = item.price
+
+            };
+
+            //return ok responce
+            return Ok(itemDTO);
+
+
+        }
+
+    }
+}
---
+++ WebApplication1/Repositories/ItemRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models.Domain;
+using WebApplication1.Models.DTO;
+
+namespace WebApplication1.Repositories
+{
+    public class ItemRepository : IitemRepository
+    {
+
+        private readonly RestDbContext restDbContext;
+
+        public ItemRepository(RestDbContext restDbContext)
+        {
+            this.restDbContext = restDbContext;
+        }
+
+        public async Task<Item> AddAsync(Item items)
+        {
+            items.itemId = Guid.NewGuid();
+            await restDbContext.AddAsync(items);
+            await restDbContext.SaveChangesAsync();
+            return items;
+        }
+
+        public async Task<Item> DeleteAsync(Guid itemID)
+        {
+            var item = await restDbContext.item.FirstOrDefaultAsync(x => x.itemId == itemID);
+
+            if (item == null)
+            {
+                return null;
+            }
+
+            // Delete the item 
+            restDbContext.item.Remove(item);
+            await restDbContext.SaveChangesAsync();
+            return item;
+        }
+
+        public async Task<IEnumerable<Item>> GetAllAsync()
+        {
+            return await restDbContext.item.ToListAsync();
+        }
+
+        public async Task<Item> GetAsync(Guid ItemID)
+        {
+            return await restDbContext.item.FirstOrDefaultAsync(x => x.itemId == ItemID);
+        }
+
+        public async Task<Item> UpdateAsync(Guid itemID, Item items)
+        {
+            var existingItem = await restDbContext.item.FirstOrDefaultAsync(x => x.itemId == itemID);
+            if (existingItem == null)
+            {
+                return null;
+            }
+
+            existingItem.name = items.name;
+            existingItem.price = items.price;
+
+            await restDbContext.SaveChangesAsync();
+
+            return existingItem;
+
+        }
+    }
+}
